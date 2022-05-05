0bdb63e369ff75c22446a2b5c590190de352b019
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-05 10:09:10 +0300

    Luhn Credit Card Algorithm

---
+++ src/main/java/app/luhn/v1/LuhnApplication.java
@@ -0,0 +1,13 @@
+package app.luhn.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuhnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuhnApplication.class, args);
+    }
+
+}
